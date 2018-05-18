7aa267dbb65077bea3848337f746df251f5b2f9f
Justin McCarthy <justin@strongdm.com>
2018-05-17 22:45:39 -0700

    only open html after render is complete during serve

--- internal/render/html.go
+++ internal/render/html.go
@@ -9,6 +9,7 @@ import (
 	"sync"
 
 	"github.com/pkg/errors"
+	"github.com/skratchdot/open-golang/open"
 	"github.com/yosssi/ace"
 )
 
@@ -29,6 +30,8 @@ const websocketReloader = `<script>
 </script>`
 
 func html(output string, live bool, errCh chan error, wg *sync.WaitGroup) {
+	opened := false
+
 	for {
 		files, err := ioutil.ReadDir(filepath.Join(".", "templates"))
 		if err != nil {
@@ -77,10 +80,17 @@ func html(output string, live bool, errCh chan error, wg *sync.WaitGroup) {
 			}
 			w.Close()
 		}
-		if !live {
+
+		if live {
+			if !opened {
+				opened = true
+				open.Run("output/index.html")
+			}
+		} else {
 			wg.Done()
 			return
 		}
+
 		<-subscribe()
 	}
 }
